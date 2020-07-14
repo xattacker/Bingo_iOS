b6bcbd48d7acbd710ba7b86bb2a1b67e8b72c3a6
xattacker <xattacker.tao@gmail.com>
2020-07-14 10:50:17 +0800

    removed unused files

--- Bingo/Classes/MacroDefine.swift
+++ Bingo/Classes/MacroDefine.swift
@@ -5,6 +5,3 @@
 //  Created by xattacker on 2016/12/15.
 //  Copyright © 2016年 xattacker. All rights reserved.
 //
-
-let OFFICIAL_WEB_SITE = "http://www.dynasty-theater.com/"
-let APP_STORE_ID = "fasfsfsadf"  // the app id must get on app store
