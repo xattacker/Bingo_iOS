3f9d86541c6acdb87e9f514b78ad90edf75e54c5
xattacker <xattacker.tao@gmail.com>
2020-07-16 11:56:12 +0800

    implmented UICountView

--- Bingo/Classes/ViewController/UICountView.swift
+++ Bingo/Classes/ViewController/UICountView.swift
@@ -27,5 +27,57 @@ class UICountView: UIView
         {
             return
         }
+        
+        
+        let offset = rect.size.width / 10
+        
+        context.setStrokeColor(UIColor.blue.cgColor)
+        context.setLineWidth(offset)
+        context.setLineJoin(CGLineJoin.round)
+        
+        if self.count >= 1
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: offset, y: offset))
+            context.addLine(to: CGPoint(x: rect.width - offset, y: offset))
+            context.closePath()
+            context.drawPath(using: CGPathDrawingMode.stroke)
+        }
+
+        if self.count >= 2
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: rect.width/2, y: offset))
+            context.addLine(to: CGPoint(x: rect.width/2, y: rect.height - offset))
+            context.closePath()
+            context.drawPath(using: CGPathDrawingMode.stroke)
+        }
+
+        if self.count >= 3
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: rect.width/2, y: rect.height/2))
+            context.addLine(to: CGPoint(x: rect.width - offset, y: rect.height/2))
+            context.closePath()
+            context.drawPath(using: CGPathDrawingMode.stroke)
+        }
+
+        if self.count >= 4
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: rect.width/4, y: (rect.height/2) - offset))
+            context.addLine(to: CGPoint(x: rect.width/4, y: rect.height - offset))
+            context.closePath()
+            context.drawPath(using: CGPathDrawingMode.stroke)
+        }
+
+        if self.count >= 5
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: offset/2, y: rect.height - offset))
+            context.addLine(to: CGPoint(x: rect.width - offset/2, y: rect.height - offset))
+            context.closePath()
+            context.drawPath(using: CGPathDrawingMode.stroke)
+        }
     }
 }
